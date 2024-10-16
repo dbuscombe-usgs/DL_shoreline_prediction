5a9005efba1fc49bafd14ded51bec372718d9aa2
Dan Buscombe <dbuscombe@gmail.com>
2024-10-16 14:43:15 -0700

    fig 6 vitousek

--- 1run_models/run_cnn_models_Hsonly_OBdata_smallestpossible.py
+++ 1run_models/run_cnn_models_Hsonly_OBdata_smallestpossible.py
@@ -19,9 +19,9 @@ from tensorflow.keras.layers import Dense,Dropout,Flatten
 from tensorflow.keras.layers import Conv1D, MaxPooling1D
 
 from keras.utils import plot_model
-# from keras.utils import plot_model
 import matplotlib.pyplot as plt
 from tensorflow.python.keras.utils.layer_utils import count_params
+
 import matplotlib.dates as mdates
 import matplotlib
 import datetime as dt
@@ -103,7 +103,62 @@ scaler_eval, mateval_out_norm = normal_data(mat_out,eval_date)
 eval = mat_eval.values.astype('float32')
 
 
+####################Define Loss functions: NEURAL NETWORK##################
+def set_seed(seed):
+    tf.random.set_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+#CNN model
+def cnn_custom(train_x, train_y, dev_x, dev_y, cfg): #, second_ccn_bank, linear_activation, num_neurons):
+    print("--------------------------------")
+    print("Model:", cfg)
+    set_seed(33)
+    # define model    # create configs
+    n_filters, n_kernels, n_drop, n_epochs,n_batch = cfg    
+    model = Sequential()
+    model.add(Conv1D(filters=n_filters, kernel_size=n_kernels,
+                    activation='relu', input_shape=(n_steps_in, n_features))) #relu
+    
+    model.add(MaxPooling1D(pool_size=2)) 
+    model.add(Flatten()) 
+    # if linear_activation is True:
+    num_neurons = 1
+    # model.add(Dense(num_neurons, activation='linear'))      
+    model.add(Dense(1))    
+
+    loss= mielke_loss
+    min_delta= 0.001
+
+    model.compile(optimizer='adam', loss=loss)
+    # fit model
+    es = EarlyStopping(patience=10, verbose=0, min_delta=min_delta,
+                    monitor='val_loss', mode='auto',
+                    restore_best_weights=True)
+    history= model.fit(train_x, train_y, validation_data=(dev_x, dev_y),
+                        batch_size=n_batch, epochs=n_epochs, verbose=2,
+                        callbacks=[es])  
+    return model, history
+
+
 # n_steps_in, n_steps_out =40,1
+
+#####
+#### a range of hyperparams, only varying kernel size
+##n_filters, n_kernels, n_drop, n_epochs,n_batch 
+cfg_list = [
+[2, 2, 0.0, 200, 8],
+[2, 4, 0.0, 200, 8],
+[2, 6, 0.0, 200, 8],
+[2, 8, 0.0, 200, 8],
+[2, 10, 0.0, 200, 8],
+[2, 12, 0.0, 200, 8]
+]
+
+# second_ccn_bank = False 
+# linear_activation = True
 
 n_steps_out =1
 
@@ -135,84 +190,15 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     #Test set, depends on study site
     test = mat_in[date_forecast:mat_in.index[-1]].values.astype('float32')
 
-
     #############################################################################
     #From pandas to array, HERE WE SEPARATE THE INPUTS FROM THE Y_OUTPUT
     # split a multivariate sequence into samples 
-
-
     train_x, train_y = split_sequences(train, n_steps_in, n_steps_out)
     dev_x, dev_y = split_sequences(dev, n_steps_in, n_steps_out)
     test_x, test_y = split_sequences(test, n_steps_in, n_steps_out)
     # # the dataset knows the number of features, e.g. 2
     n_timesteps, n_features, n_outputs = train_x.shape[1], train_x.shape[
         2], train_y.shape[1]
-
-
-    ####################Define Loss functions: NEURAL NETWORK##################
-    def set_seed(seed):
-        tf.random.set_seed(seed)
-        os.environ['PYTHONHASHSEED'] = str(seed)
-        np.random.seed(seed)
-        random.seed(seed)
-
-
-    #CNN model
-    loss= mielke_loss
-    min_delta= 0.001
-    def cnn_custom(train_x, train_y, dev_x, dev_y, cfg, second_ccn_bank, linear_activation, num_neurons):
-        print("--------------------------------")
-        print("Model:", cfg)
-        set_seed(33)
-        # define model    # create configs
-        n_filters, n_kernels, n_drop, n_epochs,n_batch = cfg    
-        model = Sequential()
-        model.add(Conv1D(filters=n_filters, kernel_size=n_kernels,
-                        activation='relu', input_shape=(n_steps_in, n_features)))
-        
-
-        if second_ccn_bank is True:
-            model.add(Conv1D(filters=n_filters, kernel_size=n_kernels,
-                        activation='relu'))
-        else:
-            print("using smaller cnn model")
-
-        model.add(MaxPooling1D(pool_size=2)) 
-        model.add(Flatten()) 
-        if linear_activation is True:
-            model.add(Dense(num_neurons, activation='linear')) 
-        else:
-            model.add(Dense(num_neurons, activation='relu')) 
-        # model.add(Dropout(n_drop))       
-        model.add(Dense(1))    
-
-        model.compile(optimizer='adam', loss=loss)
-        # fit model
-        es = EarlyStopping(patience=10, verbose=0, min_delta=min_delta,
-                        monitor='val_loss', mode='auto',
-                        restore_best_weights=True)
-        history= model.fit(train_x, train_y, validation_data=(dev_x, dev_y),
-                            batch_size=n_batch, epochs=n_epochs, verbose=2,
-                            callbacks=[es])  
-        return model, history
-
-    #####
-    num_neurons = 1
-
-    #### a range of hyperparams, only varying kernel size
-    ##n_filters, n_kernels, n_drop, n_epochs,n_batch 
-    cfg_list = [
-    [2, 2, 0.0, 200, 8],
-    [2, 4, 0.0, 200, 8],
-    [2, 6, 0.0, 200, 8],
-    [2, 8, 0.0, 200, 8],
-    [2, 10, 0.0, 200, 8],
-    [2, 12, 0.0, 200, 8]
-    ]
-
-    second_ccn_bank = False 
-    linear_activation = True
-
 
     #%Run model configurations in loop###########################################
     #Predefine empty dataframe
@@ -222,12 +208,8 @@ for n_steps_in in [20,30,40,50,60,70,80]:
                         columns=['ann1','ann2','ann3','ann4','ann5',
                                     'ann6'])
 
-    # yresults= pd.DataFrame(index=mat_in[ plot_date :mat_in.index[-1]].index,
-    #                     columns=['ann1','ann2','ann3'])
-
     #Rescale target shoreline time series
     testY = scaler.inverse_transform(test_y)
-
 
     MODELS=[]
     HISTORY=[]
@@ -236,29 +218,19 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     for (index, colname) in enumerate(yresults):
         print('Model number:' + str(index))
         #Train model with hyp config from config list
-        model,history = cnn_custom(train_x, train_y, dev_x, dev_y, cfg_list[index], second_ccn_bank, linear_activation, num_neurons) 
+        model,history = cnn_custom(train_x, train_y, dev_x, dev_y, cfg_list[index])
+        #, second_ccn_bank, linear_activation, num_neurons) 
         # print(model.summary())
 
         MODELS.append(model)
         HISTORY.append(history)
+
         trainable_count = count_params(model.trainable_weights)
         batch = cfg_list[index][-1]
         kernel = cfg_list[index][1]
         PARAMS.append(trainable_count)
 
-        plot_model(
-            model,
-            to_file=f"CNN_f_microcnn_OceanBeach_p"+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+".png",
-            show_shapes=True,
-            show_dtype=False,
-            show_layer_names=False,
-            rankdir="TB",
-            expand_nested=False,
-            dpi=200,
-            show_layer_activations=True,
-            show_trainable=True)
-
-
+        # plot_model(model,to_file=f'microcnn{index}.png')
 
         testdl = model.predict(test_x)     
         yresults.iloc[:,index]= scaler.inverse_transform(testdl)
@@ -278,23 +250,21 @@ for n_steps_in in [20,30,40,50,60,70,80]:
         print(yresults.head())
 
 
+    num_neurons = 1
 
     ##EXPORT ENSEMBLE
     yresults["obs"]=testY[:,0]
-    yresults.to_csv(f'./output/CNN_f_microcnn_OceanBeach_nn{num_neurons}_p{trainable_count}_linearact_DT{n_steps_in}.csv')
+    yresults.to_csv(f'./output/CNN_f_microcnn2_OceanBeach_nn{num_neurons}_p{trainable_count}_linearact_DT{n_steps_in}.csv')
 
     ## Metrics 
     ymetrics= pd.DataFrame(index=np.arange(7),
                         columns=['rmse_arr','pear_arr','mielke_arr'])
 
-    # ymetrics= pd.DataFrame(index=np.arange(4),
-    #                     columns=['rmse_arr','pear_arr','mielke_arr'])
-
     ymetrics['rmse_arr']=np.array([root_mean_squared_error(yresults[colname].values,testY) for (index, colname) in enumerate(yresults)])
     ymetrics['pear_arr']=np.array([scipy.stats.pearsonr(yresults[colname].values,testY[:,0])[0] for (index, colname) in enumerate(yresults)])
     ymetrics['mielke_arr']=np.array( [index_mielke(yresults[colname].values,testY[:,0]) for (index, colname) in enumerate(yresults)])
 
-    ymetrics.to_csv(f'./output/CNN_f_microcnn_OceanBeach_nn{num_neurons}_p{trainable_count}_DT{n_steps_in}_linearact_metrics.csv')
+    ymetrics.to_csv(f'./output/CNN_f_microcnn2_OceanBeach_nn{num_neurons}_p{trainable_count}_DT{n_steps_in}_linearact_metrics.csv')
 
 
     #################
@@ -325,12 +295,11 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     xl = plt.xlim()
     plt.plot(xl,xl,'r--')
     plt.title(f'Hs-only, {num_params}-parameters')
-    plt.text(20, -40, 'RMSE (test) = {best_rmse}-m')
+    plt.text(20, -40, 'RMSE (test) = '+best_rmse+'-m')
     # plt.text(65, 48, '51 trainable params.')
 
     plt.xlabel('Observed shoreline position (m)')
     plt.ylabel('Estimated shoreline position (m)')
-
 
     model_out_all = best_model.predict(eval_x)
 
@@ -349,22 +318,30 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     myFmt = mdates.DateFormatter('%Y')
     plt.gca().xaxis.set_major_formatter(myFmt)
 
-
     plt.xlabel('Time')
     plt.ylabel('Shoreline position (m)')
     # plt.show()
-    plt.savefig(f'./output/CNN_f_OceanBeach_microCNN_ts_DT{n_steps_in}_params{num_params}.png', dpi=200, bbox_inches='tight')
+    plt.savefig(f'./output/CNN_f_OceanBeach_microCNN2_ts_DT{n_steps_in}_params{num_params}.png', dpi=200, bbox_inches='tight')
     plt.close()
 
-
-
+    plot_model(
+        best_model,
+        to_file="model.png",
+        show_shapes=False,
+        show_dtype=False,
+        show_layer_names=False,
+        rankdir="TB",
+        expand_nested=False,
+        dpi=200,
+        show_layer_activations=False,
+        show_trainable=False)
 
     ## use the model as afeature extractor to examine correlation between features and outputs
 
     plt.close()
     plt.figure(figsize=(16,8))
     plt.subplots_adjust(wspace=0.3, hspace=0.3)
-    plt.subplot(211)
+    plt.subplot(311)
     dates = matplotlib.dates.date2num(obs_shoreline.index.values[:len(eval_x)])
 
     plt.plot(dates, evalY.squeeze(), lw=2, label='Observations')
@@ -373,11 +350,11 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     # plt.gcf().autofmt_xdate()
     myFmt = mdates.DateFormatter('%Y')
     plt.gca().xaxis.set_major_formatter(myFmt)
-    plt.text(dates[int(3.5*len(dates)/4)], -70, f'RMSE (test) = {best_rmse}-m')
+    plt.text(dates[int(len(dates)/2)], -50, f'RMSE (test) = {best_rmse}-m')
     plt.xlabel('Time')
     plt.ylabel('Shoreline position (m)')
 
-    ax = plt.subplot(212)
+    ax = plt.subplot(312)
     ax2 = ax.twinx()
   
     weights = Model(best_model.inputs, best_model.layers[0].output)
@@ -393,24 +370,23 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     ax.set_ylabel('Convolution layer weights (-)', color='r')
     ax2.set_ylabel('Convolution layer biases (-)', color='b')
 
-    # ax3 = plt.subplot(313)
-    # ax4 = ax3.twinx()
+    ax3 = plt.subplot(313)
+    ax4 = ax3.twinx()
 
-    # # shoreML = np.convolve(cnn_weights[:,:,0].mean(axis=1), features[:,0],'full')[:len(dates)]
-    # ax3.plot(dates,  evalY.squeeze(), lw=2, label='Observations')
-    # ax4.plot(dates, cnn_weights[:,:,0].mean(axis=1),'r--', label='CNN weights')
-    # myFmt = mdates.DateFormatter('%Y')
-    # ax3.xaxis.set_major_formatter(myFmt)
-    # plt.xlabel('Time')
+    # shoreML = np.convolve(cnn_weights[:,:,0].mean(axis=1), features[:,0],'full')[:len(dates)]
+    ax3.plot(dates,  evalY.squeeze(), lw=2, label='Observations')
+    ax4.plot(dates, cnn_weights[:,:,0].mean(axis=1),'r--', label='CNN weights')
+    myFmt = mdates.DateFormatter('%Y')
+    ax3.xaxis.set_major_formatter(myFmt)
+    plt.xlabel('Time')
 
-    # ax3.set_xlabel('Time')
-    # ax3.set_ylabel('Observed shoreline position (m)', color='b')
-    # ax4.set_ylabel('Convolution layer weights (-)', color='r')
+    ax3.set_xlabel('Time')
+    ax3.set_ylabel('Observed shoreline position (m)', color='b')
+    ax4.set_ylabel('Convolution layer weights (-)', color='r')
 
     # plt.show()
-    plt.savefig(f'./output/CNN_f_microcnn_OceanBeach_nn{num_neurons}_p'+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+'_linearact_DT'+str(n_steps_in)+'_model_explore1.png', dpi=300, bbox_inches='tight')
+    plt.savefig(f'./output/CNN_f_microcnn2_OceanBeach_nn{num_neurons}_p'+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+'_linearact_DT'+str(n_steps_in)+'_model_explore1.png', dpi=200, bbox_inches='tight')
     plt.close('all')
-
 
 
     ######
@@ -418,31 +394,31 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     features = extract.predict(train_x)
 
     ###====================================
-    plt.figure(figsize=(16,8))
+    plt.figure(figsize=(12,12))
     plt.subplots_adjust(wspace=0.3, hspace=0.3)
 
-    plt.subplot(1,3,1)
+    plt.subplot(2,2,1)
     plt.plot(mat_in['f'], obs_shoreline.values.squeeze(), '.')
     xc = np.min(np.corrcoef(mat_in['f'], obs_shoreline.values.squeeze()))
     plt.text(7.5,-30,'R='+str(xc)[:6])
     plt.xlabel('Wave height anomaly (m)')
     plt.ylabel('Shoreline position (m)')
 
-    # plt.subplot(2,2,2)
-    # plt.plot(features[:,0], scaler.inverse_transform(train_y), '.')
-    # xc = np.min(np.corrcoef(features[:,0], scaler.inverse_transform(train_y).squeeze()))
-    # plt.text(-5,-30,'R='+str(xc)[:6])
-    # plt.xlabel('Dense layer feature vector (-)')
-    # plt.ylabel('Shoreline position (m)')
+    plt.subplot(2,2,2)
+    plt.plot(features[:,0], scaler.inverse_transform(train_y), '.')
+    xc = np.min(np.corrcoef(features[:,0], scaler.inverse_transform(train_y).squeeze()))
+    plt.text(-5,-30,'R='+str(xc)[:6])
+    plt.xlabel('Dense layer feature vector (-)')
+    plt.ylabel('Shoreline position (m)')
 
-    plt.subplot(1,3,2)
+    plt.subplot(2,2,3)
     plt.plot(cnn_weights[:,:,0].mean(axis=1), evalY.squeeze(), '.')
     xc = np.min(np.corrcoef(cnn_weights[:,:,0].mean(axis=1), evalY.squeeze()))
     plt.text(3,30,'R='+str(xc)[:6])
     plt.xlabel('Convolution layer weights (-)')
     plt.ylabel('Shoreline position (m)')
 
-    plt.subplot(1,3,3)
+    plt.subplot(2,2,4)
     # plt.plot(cnn_weights[:,:,1].mean(axis=1), eval_x.mean(axis=1), '.')
     plt.plot(cnn_weights[:,:,1].mean(axis=1), rescale_array(eval_x.mean(axis=1),f.min(), f.max()).squeeze(), '.')
     # plt.plot(cnn_weights[:,:,1].mean(axis=1), (y_est + bias) - evalY.squeeze(), '.')
@@ -452,65 +428,6 @@ for n_steps_in in [20,30,40,50,60,70,80]:
     plt.ylabel('Wave height anomaly (m)')
 
     # plt.show()
-    plt.savefig(f'./output/CNN_f_microcnn_OceanBeach_nn{num_neurons}_p'+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+'_linearact_DT'+str(n_steps_in)+'_modelexplore2.png', dpi=300, bbox_inches='tight')
+    plt.savefig(f'./output/CNN_f_microcnn2_OceanBeach_nn{num_neurons}_p'+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+'_linearact_DT'+str(n_steps_in)+'_modelexplore2.png', dpi=200, bbox_inches='tight')
     plt.close()
-
-
-
-
-    plot_model(
-        best_model,
-        to_file=f"CNN_f_microcnn_OceanBeach_p"+str(trainable_count)+'b'+str(batch)+'k'+str(kernel)+'DT'+str(n_steps_in)+".png",
-        show_shapes=True,
-        show_dtype=False,
-        show_layer_names=False,
-        rankdir="TB",
-        expand_nested=False,
-        dpi=200,
-        show_layer_activations=True,
-        show_trainable=True)
-
-
-    # plt.plot(rescale_array(eval_x.mean(axis=1),f.min(), f.max()).squeeze()*cnn_weights[:,:,0].sum(axis=1) + cnn_weights[:,:,1].sum(axis=1), evalY.squeeze(), '.')
-
-
-    # plt.plot(cnn_weights[:,:,0].sum(axis=1), cnn_weights[:,:,1].sum(axis=1),'.')
-
-
-
-# layer_outputs = [] # we create the list that will contain the results of the processing of the neurons of the layer
-# # for each neuron
-# for neuron_weights, neuron_bias in zip(cnn_weights[0][:,0], cnn_weights[0][:,1]):
-#     # initialize output to 0
-#     neuron_output = 0
-#     # for each input and weight
-#     for n_input in zip(rescale_array(eval_x[0],f[:40].min(), f[:40].max()).squeeze(), ):
-#         # multiply input and weight and add it to the output
-#         neuron_output += n_input[0] * neuron_weights
-#     # add bias to the output
-#     neuron_output += neuron_bias
-#     # add the neuron result to the layer
-#     layer_outputs.append(neuron_output)
-
-# layer_outputs = np.array(layer_outputs)
-
-# cnn_weights0 = weights.predict(eval_x)[:40]
-
-
-
-# plt.figure(figsize=(8,12))
-# plt.subplot(411); plt.plot(rescale_array(eval_x[0],f[:40].min(), f[:40].max()).squeeze())
-# plt.title('wave height anomaly')
-
-# plt.subplot(412); plt.plot(cnn_weights[0][:,0])
-# plt.title('convolutional layer weights')
-
-# plt.subplot(413); plt.plot(cnn_weights[0][:,1])
-# plt.title('convolutional layer biases')
-
-# plt.subplot(414); plt.plot(layer_outputs, label='Convolution layer outputs')
-# plt.subplot(414); plt.plot(evalY.squeeze()[:40], label='Observed shoreline position')
-# plt.legend()
-# plt.savefig('temp.png'); plt.close()
-
 
